4a8e4343c66f59d6ddf9f9a1bcafca48a1bbe9f1
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-03-18 20:30:33 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro cliente</title>
+
+    <link rel="stylesheet" href="{{ asset('site/bootstrap.css') }}">
+</head>
+<body>
+    <h1>teste</h1>
+    <button class="btn btn-lg btn-success">OK</button>
+
+    <script src="{{ asset('site/jquery.js') }}"></script>
+    <script src="{{ asset('site/bootstrap.js') }}"></script>
+</body>
+</html>
